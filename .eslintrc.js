f833641c9af626b1930aea0f887012fca89c81ee
codejedi365 <codejedi365@gmail.com>
2021-08-15 23:41:23 -0400

    repo-conf > code lint/style configuration

---
+++ .eslintrc.js
@@ -0,0 +1,68 @@
+module.exports = {
+  root: true,
+  env: {
+    commonjs: true,
+    es2017: true,
+    node: true
+  },
+  extends: [
+    // JS, MD, MDX
+    "eslint:recommended",
+    "airbnb-base",
+    "plugin:mdx/recommended",
+    "plugin:prettier/recommended"
+  ],
+  settings: {
+    "mdx/code-blocks": true
+  },
+  ignorePatterns: [
+    "**/node_modules",
+    "dist/**",
+    "**/package-lock.json",
+    "**/tsconfig.json",
+    ".vscode/**",
+    "!.*"
+  ],
+  rules: {
+    "no-console": "off",
+    // webpack handles all dependencies to generate remaining bundle
+    "import/no-extraneous-dependencies": "off"
+  },
+  overrides: [
+    {
+      files: ["*.json", ".remarkrc"],
+      plugins: ["json-format"]
+    },
+    {
+      files: ["*.ts"],
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        ecmaVersion: 8,
+        project: "tsconfig.json"
+      },
+      plugins: ["@typescript-eslint"],
+      extends: [
+        "eslint:recommended",
+        "airbnb-typescript/base",
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+        "plugin:prettier/recommended"
+      ],
+      rules: {
+        "no-console": "off",
+        // webpack handles all dependencies to generate remaining bundle
+        "import/no-extraneous-dependencies": "off",
+        "no-plusplus": "off",
+        "no-nested-ternary": "warn",
+        yoda: "off"
+      }
+    },
+    {
+      files: ["**.test.ts"],
+      env: {
+        jest: true
+      }
+    }
+  ]
+};
